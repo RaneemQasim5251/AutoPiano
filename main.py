365cd4ed5af1408fcbb179d1791a175c88e0bf49
Raneem Qasim <108181104+RaneemQasim5251@users.noreply.github.com>
2023-05-23 11:35:38 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,84 @@
+########################################
+#
+# Python script to automatically play
+#    music sheets on Virtual Piano
+#    (https://virtualpiano.net/)
+#
+########################################
+
+# packages
+import pyautogui as pg
+import time
+
+# wait until user navigates to "https://virtualpiano.net/"
+print('Please navigate to "https://virtualpiano.net/"')
+print('Make sure that piano has loaded and you have')
+print('your browser window with virtual piano in focus!')
+print('Music would startplaying in 5 sec...')
+time.sleep(5)
+
+# =====================================
+# Preferred delays for different pieces
+# =====================================
+#
+# Interstellar Main Theme:          0.1
+# Yiruma - River flows in you:     0.03
+# Valse (Amélie)                    0.1
+# Comptine d'un autre été (Amélie) 0.08
+# Goblin OST                       0.06
+
+# delay between notes (change tempo)
+delay = 0.05
+
+# open music sheet
+with open('New Text Document.txt') as f:
+    # init notes
+    notes = f.read()
+
+    # init index
+    index = 0
+
+    # loop over notes
+    while index in range(len(notes)):
+        if notes[index].isalpha() or notes[index].isdigit():
+            # mimic piano key press
+            pg.press(notes[index])
+
+            # print pressed key
+            print("pressed key:", notes[index])
+
+        else:
+            # handle long notes
+            if notes[index] == '|': time.sleep(delay * 8)
+
+            # handle chords
+            if notes[index] == '[':
+                # define chord list
+                chord = []
+
+                # parse chord
+                while notes[index] != ']':
+                    # append note to chord
+                    if notes[index].isalpha() or notes[index].isdigit():
+                        # append note to chord
+                        chord.append(notes[index])
+
+                    # go to next note within a chord
+                    index += 1
+
+                # mimic piaono chord play
+                exec('pg.hotkey(' + str(chord)[1: -1] + ')')
+
+                # print pressed keys
+                print("pressed keys:", chord)
+
+        # default pause between notes
+        time.sleep(delay)
+
+        # skip to the next note
+        index += 1
+
+print('Thanks for listening!')
+
+
+
